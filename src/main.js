4fc03b1799a361f481dc0e651e7fcb82742f3bfa
Steve Mathews <stevenmathews.nz@gmail.com>
2016-01-19 15:39:44 +1300

    Remove firebase_login component

--- src/main.js
+++ src/main.js
@@ -1,21 +1,14 @@
 import countdownTimer from './countdown_timer/index'
-import firebaseLogin from './firebase_login/main'
-import view from './view'
 import {Observable} from 'rx'
 
 export default function main ({DOM}) {
-  const facebookLogin = firebaseLogin(DOM)
   const pomodoroTimer = countdownTimer({
     DOM,
     props$: Observable.of({
       startTimeMinutes: 0.05
     })
   })
-
-  const vtree$ = view(
-    facebookLogin.DOM,
-    pomodoroTimer.DOM
-  )
+  const vtree$ = pomodoroTimer.DOM
 
   return {
     DOM: vtree$,
