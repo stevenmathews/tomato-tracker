b1d73ace2082f7dcfe62da8694857c7440d2488a
Steve <stevenmathews.nz@gmail.com>
2016-02-07 19:21:54 +1300

    Use optimised logo image

--- src/prerender/layout.js
+++ src/prerender/layout.js
@@ -1,6 +1,6 @@
 module.exports = [
   '<div class="layout" style="background-color:#FAAB57;height:6em;">',
-    '<img class="logo" src="/images/logo-orange.jpeg" style="width:5em;margin-left:1em;margin-top:0.5em;">', // eslint-disable-line indent
+    '<img class="logo" src="/images/logo-orange.png" style=margin-left:1em;margin-top:0.5em;">', // eslint-disable-line indent
     '<h1 class="title" style="display:inline-block;position:relative;top:-0.9em;left:0.5em;font:2.2em Bevan, cursive;">TomatoTracker</h1>', // eslint-disable-line indent
   '</div>',
   '<div id="app">',
