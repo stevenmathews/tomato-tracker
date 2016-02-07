34f781b5b87cc57d251763c2bf43d189a3c765af
Steve <stevenmathews.nz@gmail.com>
2016-02-07 13:28:35 +1300

    Move layout styles to prerendered html

--- src/prerender/head.js
+++ src/prerender/head.js
@@ -1,5 +1,6 @@
 module.exports = [
   '<title>TomatoTracker</title>',
   '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">',
+  '<link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet" type="text/css">',
   '<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon"/>'
 ].join('')
--- src/prerender/layout.js
+++ src/prerender/layout.js
@@ -1,7 +1,7 @@
 module.exports = [
-  '<div class="layout">',
-    '<img class="logo" src="/images/logo-orange.jpeg">', // eslint-disable-line indent
-    '<h1 class="title">TomatoTracker</h1>', // eslint-disable-line indent
+  '<div class="layout" style="background-color:#FAAB57;height:6em;">',
+    '<img class="logo" src="/images/logo-orange.jpeg" style="width:5em;margin-left:1em;margin-top:0.5em;">', // eslint-disable-line indent
+    '<h1 class="title" style="display:inline-block;position:relative;top:-0.9em;left:0.5em;font:2.2em Bevan, cursive;">TomatoTracker</h1>', // eslint-disable-line indent
   '</div>',
   '<div id="app">',
   '</div>'
